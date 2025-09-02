dd65f9f070a2642fec0ccc5d311e4f5a0a7527c5
Miguel Arcos <miguel.arcos.trader@gmail.com>
2025-09-02 18:13:19 -0500

    :tada: Begin the new project

---
+++ src/main/java/com/meli/distributed_inventory_management_service/DistributedInventoryManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.meli.distributed_inventory_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedInventoryManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedInventoryManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meli/distributed_inventory_management_service/DistributedInventoryManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.distributed_inventory_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedInventoryManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
