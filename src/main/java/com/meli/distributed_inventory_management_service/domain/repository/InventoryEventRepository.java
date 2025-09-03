234897d2f7b02323ae6ab8ae16565077bc8af120
Miguel Arcos <miguel.arcos.trader@gmail.com>
2025-09-03 12:24:24 -0500

    :sparkles: Add repositories to domain

---
+++ src/main/java/com/meli/distributed_inventory_management_service/domain/repository/InventoryEventRepository.java
@@ -0,0 +1,19 @@
+package com.meli.distributed_inventory_management_service.domain.repository;
+
+import com.meli.distributed_inventory_management_service.domain.model.InventoryUpdateEvent;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface InventoryEventRepository {
+
+    Mono<InventoryUpdateEvent> save(InventoryUpdateEvent event);
+
+    Flux<InventoryUpdateEvent> findPendingEvents();
+
+    Flux<InventoryUpdateEvent> findByCorrelationId(String correlationId);
+
+    Mono<InventoryUpdateEvent> updateStatus(String eventId, String status);
+
+    Mono<InventoryUpdateEvent> findById(String eventId);
+
+}
---
+++ src/main/java/com/meli/distributed_inventory_management_service/domain/repository/InventoryRepository.java
@@ -0,0 +1,25 @@
+package com.meli.distributed_inventory_management_service.domain.repository;
+
+import com.meli.distributed_inventory_management_service.domain.model.InventoryItem;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface InventoryRepository {
+
+    Mono<InventoryItem> findById(String id);
+
+    Mono<InventoryItem> findByProductAndStore(String productId, String storeId);
+
+    Flux<InventoryItem> findByStore(String storeId);
+
+    Flux<InventoryItem> findByProduct(String productId);
+
+    Mono<InventoryItem> save(InventoryItem item);
+
+    Mono<Boolean> delete(String id);
+
+    Mono<InventoryItem> updateWithVersionCheck(InventoryItem item, Long expectedVersion);
+
+    Mono<Boolean> existsByProductAndStore(String productId, String storeId);
+
+}
