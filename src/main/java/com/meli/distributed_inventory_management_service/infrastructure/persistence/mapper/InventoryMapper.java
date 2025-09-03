459511d67f57579767133393328b7e51c6feb950
Miguel Arcos <miguel.arcos.trader@gmail.com>
2025-09-03 13:29:55 -0500

    :art: Add Inventory Mapper

---
+++ src/main/java/com/meli/distributed_inventory_management_service/infrastructure/persistence/mapper/InventoryMapper.java
@@ -0,0 +1,31 @@
+package com.meli.distributed_inventory_management_service.infrastructure.persistence.mapper;
+
+import com.meli.distributed_inventory_management_service.domain.model.InventoryItem;
+import com.meli.distributed_inventory_management_service.infrastructure.persistence.entity.InventoryEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+import org.springframework.dao.OptimisticLockingFailureException;
+
+@Mapper(componentModel = "spring")
+public interface InventoryMapper {
+
+    InventoryMapper INSTANCE = Mappers.getMapper(InventoryMapper.class);
+
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    InventoryEntity toEntity(InventoryItem domain);
+
+    InventoryItem toDomain(InventoryEntity entity);
+
+    default InventoryItem toDomainWithVersion(InventoryEntity entity, Long expectedVersion) {
+        if (!entity.getVersion().equals(expectedVersion)) {
+            throw new OptimisticLockingFailureException(
+                    "Version mismatch. Expected: " + expectedVersion +
+                            ", Actual: " + entity.getVersion()
+            );
+        }
+        return toDomain(entity);
+    }
+
+}
