fb6c468238f33b73c802e816e0176cff3f523638
Miguel Arcos <miguel.arcos.trader@gmail.com>
2025-09-03 12:22:55 -0500

    :children_crossing: Add enums

---
+++ src/main/java/com/meli/distributed_inventory_management_service/domain/model/EventStatus.java
@@ -0,0 +1,7 @@
+package com.meli.distributed_inventory_management_service.domain.model;
+
+public enum EventStatus {
+
+    PENDING, PROCESSED, FAILED, COMPENSATED, ROLLED_BACK
+
+}
---
+++ src/main/java/com/meli/distributed_inventory_management_service/domain/model/UpdateType.java
@@ -0,0 +1,7 @@
+package com.meli.distributed_inventory_management_service.domain.model;
+
+public enum UpdateType {
+
+    PURCHASE, SALE, RESTOCK, ADJUSTMENT, TRANSFER
+
+}
