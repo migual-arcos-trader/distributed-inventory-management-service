2258538f1c70c11a7fb31c9e7e966b3f315a1d44
Miguel Arcos <miguel.arcos.trader@gmail.com>
2025-09-04 15:58:01 -0500

    :wrench: Add testcontainers configs to integration tests

---
+++ src/test/java/com/meli/distributed_inventory_management_service/infrastructure/config/TestMapperConfig.java
@@ -0,0 +1,16 @@
+package com.meli.distributed_inventory_management_service.infrastructure.config;
+
+import com.meli.distributed_inventory_management_service.infrastructure.persistence.mapper.InventoryMapper;
+import org.mapstruct.factory.Mappers;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestMapperConfig {
+
+    @Bean
+    public InventoryMapper inventoryMapper() {
+        return Mappers.getMapper(InventoryMapper.class);
+    }
+
+}
