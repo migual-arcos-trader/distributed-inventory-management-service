f10a0a51a1c32f7807d82cea1be8b51ade232508
Miguel Arcos <miguel.arcos.trader@gmail.com>
2025-09-05 15:34:36 -0500

    :white_check_mark: Revert to lowercase to validate headers

--- src/main/java/com/meli/distributed_inventory_management_service/infrastructure/web/controller/AuthController.java
+++ src/main/java/com/meli/distributed_inventory_management_service/infrastructure/web/controller/AuthController.java
@@ -20,16 +20,17 @@ public class AuthController {
     private static final String ADMIN = "ADMIN";
     private static final String USER = "USER";
     private static final String PASSWORD = "password";
-    private static final String BEARER = "Bearer";
-    public static final long EXPIRES_IN = 3600L;
-
+    private static final String AUTHORIZATION = "Authorization";
+    private static final String BEARER = "Bearer ";
+    private static final int BEGIN_INDEX = 7;
+    private static final int MILLISECONDS_TO_SECOND = 1000;
     private final JwtUtil jwtUtil;
 
     @PostMapping("/login")
     public Mono<ResponseEntity<AuthResponseDTO>> login(@Valid @RequestBody AuthRequestDTO request) {
-        if (ADMIN.equalsIgnoreCase(request.username()) && PASSWORD.equals(request.password())) {
+        if (ADMIN.toLowerCase().equals(request.username()) && PASSWORD.equals(request.password())) {
             String token = jwtUtil.generateToken(request.username(), List.of(ADMIN, USER));
-            AuthResponseDTO response = new AuthResponseDTO(token, BEARER, EXPIRES_IN, request.username());
+            AuthResponseDTO response = new AuthResponseDTO(token, jwtUtil.getExpiration() / MILLISECONDS_TO_SECOND, request.username());
             return Mono.just(ResponseEntity.ok(response));
         } else {
             return Mono.just(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
@@ -37,9 +38,9 @@ public class AuthController {
     }
 
     @PostMapping("/validate")
-    public Mono<ResponseEntity<Boolean>> validateToken(@RequestHeader("Authorization") String authHeader) {
-        if (authHeader != null && authHeader.startsWith("Bearer ")) {
-            String token = authHeader.substring(7);
+    public Mono<ResponseEntity<Boolean>> validateToken(@RequestHeader(value = AUTHORIZATION, required = false) String authHeader) {
+        if (authHeader != null && authHeader.startsWith(BEARER)) {
+            String token = authHeader.substring(BEGIN_INDEX);
             boolean isValid = jwtUtil.validateToken(token);
             return Mono.just(ResponseEntity.ok(isValid));
         }
